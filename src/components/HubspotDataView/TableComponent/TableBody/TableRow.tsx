962742884a18859736f576426284dd2e39043486
Pradheep Rajendirane <pas495@gmail.com>
2022-08-31 15:54:46 +0200

    update doc + fix UI Issue with actions

--- src/components/HubspotDataView/TableComponent/TableBody/TableRow.tsx
+++ src/components/HubspotDataView/TableComponent/TableBody/TableRow.tsx
@@ -1,6 +1,5 @@
-import { DataGridCell, DataGridRow, MediaBody, MediaFigure, MediaObject, Menu, MenuButton, MenuItem, useMenuState } from '@twilio-paste/core';
+import { Button, DataGridCell, DataGridRow, Stack, useMenuState } from '@twilio-paste/core';
 import { HelpText } from '@twilio-paste/core/Help-Text';
-import { ChevronDownIcon } from "@twilio-paste/icons/esm/ChevronDownIcon";
 import { FaPhoneAlt, FaSms, FaWhatsapp } from 'react-icons/fa';
 import { ICustomer } from '../../../../Types';
 
@@ -23,35 +22,11 @@ function TableRow({ data, actionDisabled, sendSmsHandler, sendWAHandler, initiat
       <DataGridCell>{data.email ? data.email : <HelpText>N/A</HelpText>}</DataGridCell>
       <DataGridCell>{data.hs_calculated_phone_number ? data.hs_calculated_phone_number : <HelpText>N/A</HelpText>}</DataGridCell>
       <DataGridCell>
-        <MenuButton {...menu} variant="primary" disabled={actionDisabled}>
-          Actions<ChevronDownIcon decorative />
-        </MenuButton>
-        <Menu {...menu} aria-label="Actions">
-          <MenuItem {...menu} disabled={actionDisabled} onClick={() => initiateCallHandler(data)}>
-            <MediaObject verticalAlign="center">
-              <MediaFigure spacing="space20">
-                <FaPhoneAlt />
-              </MediaFigure>
-              <MediaBody>Phone Call</MediaBody>
-            </MediaObject>
-          </MenuItem>
-          <MenuItem {...menu} disabled={actionDisabled} onClick={() => { sendSmsHandler(data) }}>
-            <MediaObject verticalAlign="center">
-              <MediaFigure spacing="space20">
-                <FaSms />
-              </MediaFigure>
-              <MediaBody>SMS Message</MediaBody>
-            </MediaObject>
-          </MenuItem>
-          <MenuItem {...menu} disabled={actionDisabled} onClick={() => { sendWAHandler(data) }}>
-            <MediaObject verticalAlign="center">
-              <MediaFigure spacing="space20">
-                <FaWhatsapp />
-              </MediaFigure>
-              <MediaBody>WhatsApp Message</MediaBody>
-            </MediaObject>
-          </MenuItem>
-        </Menu>
+        <Stack orientation="horizontal" spacing="space30" >
+          <Button variant="primary" size="small" disabled={actionDisabled} onClick={() => initiateCallHandler(data)}><FaPhoneAlt /> Call</Button>
+          <Button variant="primary" size="small" disabled={actionDisabled} onClick={() => sendSmsHandler(data)}><FaSms /> SMS</Button>
+          <Button variant="primary" size="small" disabled={actionDisabled} onClick={() => sendWAHandler(data)}><FaWhatsapp /> WhatsApp</Button>
+        </Stack>
       </DataGridCell>
     </DataGridRow>
   )
